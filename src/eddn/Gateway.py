591c84e54b7c4a819d0e153a192be0df7926e062
Athanasius <github@miggy.org>
2021-11-04 13:26:37 +0000

    Gateway: typing: get_decompressed_message()

--- src/eddn/Gateway.py
+++ src/eddn/Gateway.py
@@ -75,18 +75,18 @@ def push_message(parsed_message: Dict, topic: str) -> None:
     stats_collector.tally("outbound")
 
 
-def get_remote_address():
+def get_remote_address() -> str:
     """
     Determine the address of the uploading client.
 
     First checks the for proxy-forwarded headers, then falls back to
     request.remote_addr.
-    :rtype: str
+    :returns: Best attempt at remote address.
     """
     return request.headers.get('X-Forwarded-For', request.remote_addr)
 
 
-def get_decompressed_message():
+def get_decompressed_message() -> bytes:
     """
     Detect gzip Content-Encoding headers and de-compress on the fly.
 
@@ -115,7 +115,7 @@ def get_decompressed_message():
             # This is a form-encoded POST. The value of the data attrib will
             # be the body we're looking for.
             try:
-                message_body = form_enc_parsed['data'][0]
+                message_body = form_enc_parsed[b'data'][0]
 
             except (KeyError, IndexError):
                 raise MalformedUploadError(
